d44d56e62ae3776690e24ea955c3015a68e24e3b
digevil <digevil@gmail.com>
2023-03-29 21:11:38 +0800

    fix benchMurmur3 doc

--- l-jmh/src/main/java/org/digevil/sandbox/l/jmh/hash/BenchHash.java
+++ l-jmh/src/main/java/org/digevil/sandbox/l/jmh/hash/BenchHash.java
@@ -8,24 +8,22 @@ import org.openjdk.jmh.annotations.Mode;
 import java.nio.charset.Charset;
 
 /**
- * ref: https://www.baeldung.com/java-microbenchmark-harness
+ * ref: <a href="https://www.baeldung.com/java-microbenchmark-harness">...</a>
  */
 public class BenchHash {
     /**
-     * @Benchmark indicates that this method will be measured by jmh
-     */
-    @Benchmark
-    /**
-     * @BenchmarkMode indicates what kind of benchmark to be run, should be one of the following:
+     * {@code @Benchmark} indicates that this method will be measured by jmh
+     * <p>
+     * {@code @BenchmarkMode} indicates what kind of benchmark to be run, should be one of the following:
      * - AverageTime
      * - Throughput
      * - SampleTime
      * - SingleShotTime
+     * <p>
+     * {@code @Fork} indicates that how many round and how many warmups to go
      */
+    @Benchmark
     @BenchmarkMode(Mode.Throughput)
-    /**
-     * @Fork indicates that how many round and how many warmups to go
-     */
     @Fork(value = 1, warmups = 1)
     public void benchMurmur3_128(HashExecutionPlan plan) {
         for (int i = plan.iterations; i > 0; i--) {
