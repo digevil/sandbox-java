8e6e6b374a206a85456ff35ce31bc30641d2c3d5
digevil <digevil@gmail.com>
2023-09-04 16:53:32 +0800

    s-boot3-user add user insert api

--- s-boot3-user/src/main/java/org/digevil/sboot3user/web/UserController.java
+++ s-boot3-user/src/main/java/org/digevil/sboot3user/web/UserController.java
@@ -7,10 +7,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.digevil.sandbox.s.model.User;
 import org.digevil.sboot3user.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User API Controller", description = "用户API")
 @RestController
@@ -23,8 +21,17 @@ public class UserController {
     @Operation(summary = "根据用户id获取用户详情", description = "根据用户id返回单个用户对象",
             parameters = {@Parameter(name = "id", description = "用户id")})
     @ApiResponse(responseCode = "2xx", description = "用户对象")
-    @GetMapping(value = "/find/{id}", produces = "application/json")
+    @GetMapping(value = "/find/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public User findById(@PathVariable int id) {
         return userService.findById(id);
+    }
+
+    @Operation(summary = "新建用户", description = "新建用户对象",
+            parameters = {@Parameter(name = "user", description = "用户实体对象")})
+    @PostMapping(value = "/insert", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public User insert(@RequestBody User user) {
+        userService.insert(user);
+        return user;
     }
 }
